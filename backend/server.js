3c4ed1d6d8f7ade61d8d94844f63f922e95e524d
Suraj Upadhye <s.upadhye6782@gmail.com>
2026-02-18 11:08:59 +0530

    Initialize backend with Express server and basic routes

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello Suraj 🚀 Your Express App is Running!");
+});
+
+app.get("/about", (req, res) => {
+  res.json({ message: "This is About Route" });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
